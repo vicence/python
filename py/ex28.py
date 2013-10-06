8bf62ba7fdf0cbbb2511a506b6dc2d48089d337f
vicence <tessvicence@gmail.com>
2013-10-06 22:27:03 +0800

    ww

---
+++ py/ex28.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# coding:utf-8
+
+print True and True
+print True, '\n'
+print False and True
+print False, '\n'
+print 1 == 1 and 2 == 1
+print False
+print 
+print "test" == "test"
+print True, '\n'
+print 1 == 1 or 2 != 1
+print True
+print 
+print True and 1 == 1
+print True, '\n'
+print False and 0 != 0
+print False, '\n'
+print True or 1 == 1
+print True, '\n'
+print "test" == "testing"
+print False, '\n'
+print 1 != 0 and 2 == 1
+print False, '\n'
+print "test" != "testing"
+print True, '\n'
+print "test" == 1
+print False, '\n'
+print not (True and False)
+print True, '\n'
+print not (1 == 1 and 0 != 1)
+print False, '\n'
+print not (10 == 1 or 1000 == 1000)
+print False, '\n'
+print not (1 != 10 or 3 ==4)
+print False, '\n'
+print not ("testing" == "testing" and "Zed" == "Cool Guy")
+print True, '\n'
+print 1 == 1 and not ("testing" == 1 or 1 == 0)
+print True, '\n'
+print 'chunky' == 'bacon' and not (3 == 4 or 3 == 3)
+print False, '\n'
+print 3 == 3 and not ("testing" == "testing" or "Python" == 'Fun')
+print False, '\n'
